0f262300f8505a291ce750853d39b217c89c02ab
Andrew <andrew.mk4@yahoo.com>
2021-09-22 22:11:11 -0700

    contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,6 +6,26 @@ const Contact = () => (
     <Layout>
         <Seo title="Contact" />
             <h1>Get in touch with Eli'</h1>
+            <form method="post" action="#">
+                <label>
+                    Name
+                    <input type="text" name="name" id="name" />
+                </label>
+                <label>
+                    Email
+                    <input type="email" name="email" id="email" />
+                </label>
+                <label>
+                    Subject
+                    <input type="text" name="subject" id="subject" />
+                </label>
+                <label>
+                    Message
+                    <textarea name="message" id="message" rows="5" />
+                </label>
+                <button type="submit">Send</button>
+                <input type="reset" value="Clear" />
+            </form>
     </Layout>
 )
 
