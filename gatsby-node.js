f9fc92ddd05e744cf36906af99092fc50b0004a0
Andrew <andrew.mk4@yahoo.com>
2021-09-29 02:39:40 -0700

    yarn add reach router installed

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,6 +6,19 @@
 
 // You can delete this file if you're not using it
 
+//Implement the Gatsby API "onCreatePage". This is called after every page is created.
+exports.onCreatePage = async ({ page, actions }) => {
+  const { createPage } = actions
+
+  // page.matchPath is a special key that's used for matching pages only on the client.
+  if (page.path.match(/^\/account/)) {
+    page.matchPath = "/account/*"
+
+    // Update the page.
+    createPage(page)
+  }
+}
+
 const path = require(`path`)
 
 exports.createPages = ({ graphql, actions }) => {
