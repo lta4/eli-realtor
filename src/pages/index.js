2a3c36acf857229536ae56da2c90bfc0e6c11c10
Andrew <andrew.mk4@yahoo.com>
2021-09-29 02:20:27 -0700

    begin user authentication

--- src/pages/index.js
+++ src/pages/index.js
@@ -47,6 +47,7 @@ const IndexPage = () => {
         style={{ marginBottom: `1.45rem` }}
       /> */}
       <p>
+        <Link to="/account">Go to your account</Link>
         <Link to="/page-2/">Go to page 2</Link> <br />
         <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
       </p>
