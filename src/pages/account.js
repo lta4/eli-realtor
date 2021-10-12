f9e8bb134409dfbdbc460769207d6a21448fe93d
Andrew <andrew.mk4@yahoo.com>
2021-10-12 02:55:34 -0700

    bones refining

--- src/pages/account.js
+++ src/pages/account.js
@@ -2,7 +2,7 @@ import * as React from "react"
 import { Router } from "@reach/router"
 import { Link } from "gatsby"
 import { login, logout, isAuthenticated, getProfile } from "../utils/auth"
-import Layout from "../components/layout"
+// import Layout from "../components/layout"
 import Seo from "../components/seo"
 import "../components/header.css"
 import About from "../pages/about"
@@ -23,45 +23,41 @@ const Account = ({ siteTitle }) => {
   const user = getProfile()
 
   return (
-    // <Layout>
       <>
-      <Seo title="Home" />
-      <nav>
-            <Link to="/account/">Home</Link>{" "}
-            <Link id="aboutL" to="/account/about/">About</Link>{" "}
-            <Link id="approachL" to="/account/approach/">Our Approach</Link>{" "}
-            <Link id="videoL" to="/account/video/">Videos</Link>{" "}
-            <Link id="listL" to="/account/list/">Listings</Link>{" "}
-            <Link id="caseL" to="/account/case/">Case Studies</Link>{" "}
-            <Link id="reviewL" to="/account/review/">Reviews</Link>{" "}
-            <Link id="contactL" to="/account/contact/">Contact</Link>{" "}
-            <a href ="#logout" onClick={e => {
-              logout() 
-              e.preventDefault()
-            }}
-          >
-            Log Out
-          </a>
-      </nav>
-      {/* <h1>Hi, {user.name ? user.name : "friend"}! Or should I say, potential new homeowner.</h1>
-      <p>Welcome to Eli's page.</p> */}
-      <Router>
-          <Home path="/account/" user={user} />
-          <About path="/account/about" />
-          <Approach path="/account/approach" />
-          <Video path="/account/video" />
-          <Case path="/account/case" />
-          <Contact path="/account/contact" />
-          <List path="/account/list" />
-          <Review path="/account/review" />
-      </Router>
-      {/* <p>
-        <Link to="/account">Go to your account</Link>
-        <Link to="/page-2/">Go to page 2</Link> <br />
-        <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-      </p> */}
+        <Seo title="Home" />
+        <nav>
+              <Link id="homeL" to="/account/">Home</Link>{" "}
+              <Link id="aboutL" to="/account/about/">About</Link>{" "}
+              <Link id="approachL" to="/account/approach/">Our Approach</Link>{" "}
+              <Link id="videoL" to="/account/video/">Videos</Link>{" "}
+              <Link id="listL" to="/account/list/">Listings</Link>{" "}
+              <Link id="caseL" to="/account/case/">Case Studies</Link>{" "}
+              <Link id="reviewL" to="/account/review/">Reviews</Link>{" "}
+              <Link id="contactL" to="/account/contact/">Contact</Link>{" "}
+              <a href="#logout" id="logout" onClick={e => {
+                logout() 
+                e.preventDefault()
+              }}
+            >
+              Log Out
+            </a>
+        </nav>
+        <Router>
+            <Home path="/account/" user={user} />
+            <About path="/account/about" />
+            <Approach path="/account/approach" />
+            <Video path="/account/video" />
+            <Case path="/account/case" />
+            <Contact path="/account/contact" />
+            <List path="/account/list" />
+            <Review path="/account/review" />
+        </Router>
+        {/* <p>
+          <Link to="/account">Go to your account</Link>
+          <Link to="/page-2/">Go to page 2</Link> <br />
+          <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+        </p> */}
       </>
-    // </Layout>
   )
 }
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,15 @@
 import * as React from "react"
+import { getProfile } from "../utils/auth"
 
 const Home = () => {
+
+    const user = getProfile()
+
     return (
-        <h1>home page yo</h1>
+        <>
+            <h1>Hi, {user.name ? user.name : "friend"}! Or should I say... new potential homeowner.</h1>
+            <p>Welcome to Eli's page.</p>
+        </>
     )
 }
 
